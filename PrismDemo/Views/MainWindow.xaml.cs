22b1e1a9c20edb0f18164840d619b20e3bac88ee
Marc Mansilla <mmansilla@irisad.local>
2018-07-17 12:33:20 +0200

    base project

---
+++ PrismDemo/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace PrismDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PrismDemo/Views/ViewA.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+
+namespace PrismDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for ViewA.xaml
+    /// </summary>
+    public partial class ViewA : UserControl
+    {
+        public ViewA()
+        {
+            InitializeComponent();
+        }
+    }
+}
